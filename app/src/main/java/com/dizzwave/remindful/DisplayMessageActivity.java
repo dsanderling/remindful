a1da609259588964b23baa519f4e797931e4675b
dsanderling <dsanderling@gmail.com>
2017-01-25 22:55:44 -0800

    First little updates

---
+++ app/src/main/java/com/dizzwave/remindful/DisplayMessageActivity.java
@@ -0,0 +1,63 @@
+package com.dizzwave.remindful;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(com.dizzwave.remindful.MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+
+    public void createNotification(View view){
+        //get the text
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(com.dizzwave.remindful.MainActivity.EXTRA_MESSAGE);
+
+        storeMessage(message);
+
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(17301504)
+                        .setContentTitle("My notification")
+                        .setContentText(message);
+
+        // Sets an ID for the notification
+        Random random = new Random();
+        int mNotificationId = random.nextInt(9999 - 1000) + 1000;
+        //int mNotificationId = 001;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    private void storeMessage(String message){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("com.dizzwave.remindful.myKey", message);
+        editor.commit();
+    }
+}
--- app/src/main/java/com/dizzwave/remindful/MainActivity.java
+++ app/src/main/java/com/dizzwave/remindful/MainActivity.java
@@ -1,13 +1,62 @@
 package com.dizzwave.remindful;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.Console;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Set<String> messages = getMessages();
+        renderMessages(messages);
+    }
+
+    private void renderMessages(Set<String> messages){
+        for(String message : messages){
+            Log.d("dave",message);
+            //render cell
+        }
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+
+        storeMessage(message);
+        renderMessages(getMessages());
+    }
+
+    private void storeMessage(String message){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        Set<String> messages = new HashSet<String>(sharedPref.getStringSet("com.dizzwave.remindful.messages", new HashSet<String>()));
+        messages.add(message);
+        editor.putStringSet("com.dizzwave.remindful.messages", messages);
+        editor.commit();
+    }
+
+    private Set<String> getMessages(){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        Set<String> messages = sharedPref.getStringSet("com.dizzwave.remindful.messages", new HashSet<String>());
+        return messages;
     }
 }
